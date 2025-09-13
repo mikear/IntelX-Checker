1a649392dcaa0d8e940527e06a251d738adc3d79
Diego A. Rabalo <diego_rabalo@hotmail.com>
2025-09-13 18:03:13 -0300

    Eliminación de archivos y carpetas innecesarios

--- backup/__init__.py
+++
@@ -1,9 +0,0 @@
-# Paquete intelx
-
-from .interactive_report import generate_interactive_html_report
-from .exports import export_to_interactive_html
-
-__all__ = [
-    'generate_interactive_html_report',
-    'export_to_interactive_html'
-]
